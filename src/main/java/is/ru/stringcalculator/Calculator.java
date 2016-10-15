571d4209a247f5a1ae6597cf77a930ecf891d73a
Búi Eymundsson <Bi Eymundsson>
2016-10-15 21:03:42 +0000

    2 og fleiri tölur virka

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -6,14 +6,28 @@ public class Calculator {
 		if(input.equals("")){
 			return 0;
 		}
+		else if (input.contains(",")) {
+			return sum(splitNumbers(input));
+		}
 		else{
 			return convertToInt(input);
 		}
 	}
 
 	private static int convertToInt(String number){
-	return Integer.parseInt(number);
+		return Integer.parseInt(number);
+	}
 
+	private static int sum(String[] numbers){
+ 		int total = 0;
+    	for(String number : numbers){
+			total = total + convertToInt(number);
+		}
+		return total;
+	}
+
+	private static String[] splitNumbers(String numbers){
+	    return numbers.split(",");
 	}
 }
 
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -14,4 +14,24 @@ public class CalculatorTest {
 	public void testOneNumber() {
 		assertEquals(2, Calculator.add("2"));
 	}
+
+	@Test
+	public void testTwoNumbers() {
+		assertEquals(5, Calculator.add("3,2"));
+	}
+
+	@Test
+    public void testThreeNumbers(){
+    	assertEquals(7, Calculator.add("4,1,2"));
+    }
+
+    @Test
+    public void testFourNumbers(){
+    	assertEquals(14, Calculator.add("5,2,3,4"));
+    }
+
+    @Test
+    public void testNineNumbers(){
+    	assertEquals(46, Calculator.add("4,6,8,3,4,5,6,1,9"));
+    }
 }
